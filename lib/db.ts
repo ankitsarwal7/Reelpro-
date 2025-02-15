db391399bc40700f50af03be550970ac4f60eeed
ankitsarwal7 <ankitsarwal7@gmail.com>
2025-02-15 22:51:12 +0530

    db connection

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,41 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
+// import { buffer } from "stream/consumers";
 
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.MONGODB_URI!;
+
+if(!MONGODB_URI){
+    throw new Error("please define mongodb URI in env file ");
+}
+
+let cached = global.mongoose
+
+if(!cached) {
+    cached = global.mongoose = { conn: null, promise: null};
+}   
+
+export async function connectToDatabase() {
+
+    if(cached.conn){
+        return cached.conn
+    }
+
+    if(!cached.promise){
+        const opts = {
+            bufferCommands: true,
+            maxPoolSize: 10
+        }
+        cached.promise = mongoose
+        .connect(MONGODB_URI, opts)
+        .then(() => mongoose.connection)
+ 
+    }
+    try {
+        cached.conn = await cached.promise
+    } catch (error) {
+        cached.promise = null;
+        throw error;
+    }
+
+    return cached.conn;
+  
+}
